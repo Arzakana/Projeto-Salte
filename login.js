5036d4823141c87047b083d775e99f9d205955fc
Andre Torres <131924114+Andre-Carmo@users.noreply.github.com>
2023-12-05 22:45:22 -0300

    First commit

---
+++ login.js
@@ -0,0 +1,57 @@
+
+var btnSignin = document.querySelector("#signin");
+var btnSignup = document.querySelector("#signup");
+
+var body = document.querySelector("body");
+
+
+btnSignin.addEventListener("click", function () {
+   body.className = "sign-in-js"; 
+});
+
+btnSignup.addEventListener("click", function () {
+    body.className = "sign-up-js";
+})
+
+function Register(name = "", email = "", password = "") {
+  this._name = name
+  this._email = email
+  this._password = password
+}
+
+var user 
+
+function erro(tag) {
+  document.querySelector(tag).style.display = "block"
+}
+
+
+
+function register() {
+  const name = document.querySelector("#register-name").value;
+  const email = document.querySelector("#register-email").value;
+  const password = document.querySelector("#register-password").value;
+
+  user = new Register(name, email, password)
+
+  if (user._email == "" || user._name == "" || user._password == "") {
+    erro("#erro-register")
+  }else {
+    body.className = "sign-in-js"; 
+  }
+}
+
+function login() {
+  const email = document.querySelector("#login-email").value;
+  const password = document.querySelector("#login-password").value;
+  
+  if (email === user._email && password === user._password){
+    window.location = ".\src\home.html"
+  }else if (email == "" || password == ""){
+    erro("#erro-login")
+  }
+}
+
+
+
+
